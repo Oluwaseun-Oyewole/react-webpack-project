a9144e680bad0a99ad14a6e7107b86a571f4f246
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2023-07-15 05:59:56 +0100

    css, sass, postcss loaders

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./styles/index.scss";
+
 const elp = {
   fod: "2",
   loo: 10,
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 mode = "development";
+
 if (process.env.NODE_ENV === "production") {
   mode = "production";
 }
@@ -18,6 +21,18 @@ module.exports = {
         exclude: /node_modules/,
         use: [{ loader: "babel-loader" }],
       },
+
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        // test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
     ],
   },
+  plugins: [new MiniCssExtractPlugin()],
 };
